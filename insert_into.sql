846573d9310a5da657574fffb5171c51351bdc33
gougou1993 <1478168700@qq.com>
2017-12-29 21:22:58 +0800

    Create insert_into.sql

---
+++ insert_into.sql
@@ -0,0 +1,6 @@
+# insert into my_table
+insert into my_table(id,name) values(null,'name1');
+insert into my_table(id,name) values(null,'name2');
+insert into my_table(id,name) values(null,'name3');
+insert into my_table(id,name) values(null,'name4');
+insert into my_table(id,name) values(null,'name5');
