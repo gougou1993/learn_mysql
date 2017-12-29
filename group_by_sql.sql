9cd60fcc9b62c32b91f7563261b4dfda9aa2ea04
gougou1993 <1478168700@qq.com>
2017-12-29 21:40:08 +0800

    Create group_by_sql.sql
    分组函数与havin子句

---
+++ group_by_sql.sql
@@ -0,0 +1,9 @@
+select field1,field2,aggregat_function1,aggregat_function2,
+  aggregat_function3
+from table_name
+[where condition]
+group  by
+field1,field2
+[having aggregat_function1 operater condition1 ,aggregat_function2 operater condition2]
+
+
