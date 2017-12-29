1b4328d192422c58b688349a8d4ec78647828494
gougou1993 <1478168700@qq.com>
2017-12-29 21:30:24 +0800

    Create select_sql.sql
    select语句

---
+++ select_sql.sql
@@ -0,0 +1,11 @@
+# 查询 table_name 所有的字段
+select * from table_name [where condition]
+
+# 查询单列
+select column from table_name [where condition1[,condition2,...conditionn]]
+
+# 查询多列
+select column1,column2,...,columnn from table_name [where condition1]
+
+# 给列取个别名
+select column1 as 别名 from table_name [where condition]
